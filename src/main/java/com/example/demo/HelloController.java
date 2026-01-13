a4eaf75f7f4c324d0a7b66d0db8ca39a967e9a5e
Ankur Mittal <ankurmittal9081@gmail.com>
2026-01-13 15:09:04 +0530

    Initial Spring Boot project

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hello {
+public class HelloController {
 
     @GetMapping("/home")
     public String home(){
