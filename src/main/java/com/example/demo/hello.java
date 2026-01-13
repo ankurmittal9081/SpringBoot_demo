05d2b66a2b2372e5211317c8de175c94c6ab778a
Ankur Mittal <ankurmittal9081@gmail.com>
2026-01-13 15:06:55 +0530

    Initial Spring Boot project

--- src/main/java/com/example/demo/hello.java
+++ src/main/java/com/example/demo/hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
 
     @GetMapping("/home")
-    public String Hello(){
+    public String home(){
         return "This is the First Line";
     }
     @GetMapping("/about")
